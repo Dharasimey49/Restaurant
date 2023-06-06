a19107c19ed8d0f03476735bc7923f7d71080179
favour <akinremidarasimi49@gmail.com>
2023-06-06 16:56:12 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+'use strict'
+// const order = document.querySelector("order")
+// const plusButton = document.querySelector(".icon");
+// plusButton.addEventListener("click",function(){
+// order.classList.add('hidden')
+// })
+const order = document.querySelector("order")
+const plusButton = document.querySelector(".icon");
+plusButton.addEventListener("click",function(){
+document.querySelector(".order").style.display="block"
+})
